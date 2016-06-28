05117437a2ed432bc975e094a41b4b75d1320a1b
Aerths <andruso11@gmail.com>
2016-06-28 18:35:33 -0300

    nasd

---
+++ maximo_descenso.m
@@ -0,0 +1,16 @@
+function [ xf, z, m, time ] = maximo_descenso( f, x0, eps, n )
+    tic;
+    k = 0 ;
+    gradient_f = eval(subs(jacobian(f),argnames(f),x0));
+    while ( norm(gradient_f)>eps && k<=n )
+        d= -eval(subs(jacobian(f),argnames(f),x0));
+        x0=x0+paso(f,x0)*d
+        gradient_f = eval(subs(jacobian(f),argnames(f),x0));
+        k=k+1;
+    end
+    time=toc-tic;
+    xf = x0 ;
+    z = eval(subs(f,argnames(f),x0));
+    m = k ;
+end
+
