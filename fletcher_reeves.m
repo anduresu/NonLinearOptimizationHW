fdef9b68de2cb5f44cfffb3e268cb5d4e5604ed7
Aerths <andruso11@gmail.com>
2016-07-01 20:16:19 -0300

    final algorithm

--- fletcher_reeves.m
+++ fletcher_reeves.m
@@ -4,21 +4,23 @@ function [ xf, z , m , time] = fletcher_reeves( f, x0, eps, n )
     d = -eval(subs(jacobian(f),argnames(f),x0)) ; 
     x1 = x0 + paso(f,x0)*d ;
     dif = 1 ;
-    while ( dif > -eps && norm(x1-x0) > eps && k <= n )
-        grad0 = eval(subs(jacobian(f),argnames(f),x0));
-        grad1 = eval(subs(jacobian(f),argnames(f),x1)) ;
-        rho = ( (grad1.')*(grad1) )./( (grad0.')*(grad0) ) ;
-        d = -grad1 + (rho*d.').' ;
-        k = k+1 ; 
+    grad0 = -eval(subs(jacobian(f),argnames(f),x0));
+    grad1 = -eval(subs(jacobian(f),argnames(f),x1)) ;    
+    while ( norm(x1-x0) > eps && k <= n )
+        grad0 = -eval(subs(jacobian(f),argnames(f),x0)) ;
+        grad1 = -eval(subs(jacobian(f),argnames(f),x1)) ;
+        rho = dot(grad1-grad0,grad1)/dot(grad0,grad0) ;
+        d = grad1 + (rho*d.').' ;
+        k = k+1 ;
         x0 = x1 ;
         x1 = x0 + paso(f,x0)*d ;
-        dif = eval(subs(f,argnames(f),x0)) - eval(subs(f,argnames(f),x1)) ;
         
     end
+
     time = toc ;
     xf = x0 ;
     z = eval(subs(f,argnames(f),x0)) ;
-    m = k ;
+    m = k-1 ;
 
 end
 
